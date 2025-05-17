6f6bec27833f0daca5f41948a3ef43a82f5f2278
yousseflaamim <yousseflamin99@gmail.com>
2025-05-17 11:55:06 +0200

    add firbase and file delagt

---
+++ TheCursedMap/app/TheCursedMapApp.swift
@@ -0,0 +1,26 @@
+//
+//  TheCursedMapApp.swift
+//  TheCursedMap
+//
+//  Created by Alexander Malmqvist on 2025-05-14.
+//
+
+import SwiftUI
+import FirebaseCore
+
+@main
+struct TheCursedMapApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
